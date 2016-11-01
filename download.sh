16bcaf6ee548ed60aa72b0b4ac06dde8486eeef7
zhusz <zhshzhutah2@gmail.com>
2016-11-02 01:10:15 +0800

    typo fixed

--- download.sh
+++ download.sh
@@ -15,6 +15,6 @@ cd demo_release
 wget https://www.dropbox.com/s/104v59m461g7ifu/off_the_shelf_models.tar.gz
 tar -xzf off_the_shelf_models.tar.gz
 cd ..
-wget https://www.dropbox.com/s/y7j7fl0vlbl4lae/data_release.tar.gz
-tar -xzf data_release.tar.gz
+wget https://www.dropbox.com/s/uublf9208zrx9p6/data_release_noG2.tar.gz
+tar -xzf data_release_noG2.tar.gz
 
