bd2de0f844900edac691b14959eacfdc8353ecc7
zhusz <zhshzhutah2@gmail.com>
2017-10-12 06:05:14 +0800

    2: Add download data link

---
+++ download.sh
@@ -0,0 +1,8 @@
+cd demo_release
+wget https://www.dropbox.com/s/cq3tbls7g7oey2c/off_the_shelf_model.tar.gz
+tar -xzf off_the_shelf_model.tar.gz
+cd ..
+wget https://www.dropbox.com/s/y7j7fl0vlbl4lae/data_release.tar.gz
+tar -xzf data_release.tar.gz
+
+
