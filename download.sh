4093792b40b81644842e5aabc79c5af9e8463046
zhusz <zhshzhutah2@gmail.com>
2017-10-29 19:26:46 +0800

    19: add the new version of the download script

---
+++ download.sh
@@ -0,0 +1,20 @@
+cd sr1
+mkdir sr1
+cd ..
+cd ih1
+mkdir ih1
+cd ..
+cd ih1_skip
+mkdir ih1
+cd ..
+cd ih1_p2p
+mkdir ih1
+cd ..
+
+cd demo_release
+wget https://www.dropbox.com/s/104v59m461g7ifu/off_the_shelf_models.tar.gz
+tar -xzf off_the_shelf_models.tar.gz
+cd ..
+wget https://www.dropbox.com/s/y7j7fl0vlbl4lae/data_release.tar.gz
+tar -xzf data_release.tar.gz
+
